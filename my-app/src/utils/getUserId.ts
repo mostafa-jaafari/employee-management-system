d96c4a6264ec58b232770a1df10dbf9c9fe3909e
MostafaJR1 <mostafajaafari.me@gmail.com>
2026-02-03 22:45:03 +0000

    .

--- my-app/src/utils/getUserId.ts
+++ my-app/src/utils/getUserId.ts
@@ -3,5 +3,5 @@ import { createSupabaseServerClient } from "@/lib/supabase/server";
 export async function getUserId() {
   const supabase = await createSupabaseServerClient();
   const { data: { user } } = await supabase.auth.getUser();
-  return { userId: user?.id, UserEmail: user?.email || null};
+  return user?.id || null;
 }
